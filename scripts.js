e72122869723ca8c217327106538384434a3e772
CoderAnash <150787961+codaWallah@users.noreply.github.com>
2023-11-20 15:30:11 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
